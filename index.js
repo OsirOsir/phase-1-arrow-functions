17898d4d00fff3b69ab04a882f36f7233eaa4c26
Philip osir Otieno <philiposir2@gmail.com>
2024-06-27 20:27:02 +0300

    updates on arrow functions

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const divide = (a, b) => a / b;
 const square = x => x * x;
 
 const add = (a, b) => a + b;
+
+
+
+// const nums = [1, 2, 3];
+
+// const squares = nums.map(x => x ** 2);
+
+// squares,
